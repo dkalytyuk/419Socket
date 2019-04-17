30af856861f48e333a9b17a7a7f9964d739cc95d
dkalytyuk <davidkalytyuk@gmail.com>
2019-04-17 17:42:15 -0400

    Added ability to specify port/address, and termiante server from client.

--- src/Client.java
+++ src/Client.java
@@ -7,59 +7,73 @@ import java.net.UnknownHostException;
 import java.util.Scanner;
 
 /**
- * This class implements java socket client
+ * This class implements java Socket socket
+ * Sends a message to a server to test for palindrome property (
+ * @author David Kalytyuk
  * @author pankaj
- * 
- *
+ * original code from:
+ * https://www.journaldev.com/741/java-socket-programming-server-client
+ * modified for purposes of assignment
  */
 public class Client {
 
     public static void main(String[] args) throws UnknownHostException, IOException, ClassNotFoundException, InterruptedException{
         //get the localhost IP address, if server is running on some other IP, you need to use that
         InetAddress host = InetAddress.getLocalHost();
+        int port = 1200; //default port
+        
+        if(args.length != 0) {
+	    	host = InetAddress.getByName(args[1]);
+	    	port = Integer.parseInt(args[0].substring(args[0].length() - 4));
+	    	if(port < 1024) {
+	    		System.out.println("\nCannot use ports below 1024, try again.");
+	    		System.exit(1);
+	    	} //specified port must be greater than 1024
+        } //specified port and address condition
+    	
+        System.out.println("\nServer Name: " + host.getHostName() + 
+        					"\nServer Address: " + host.getHostAddress() + 
+        					"\nPort Number: " + port + "\n");
+        
         Socket socket = null;
         ObjectOutputStream oos = null; //gets sent to server
         ObjectInputStream ois = null; //received from server
-        
         Scanner sc = new Scanner(System.in);
+        
         while(true) {
             System.out.println("Please enter text to send to server: ");
             String text = sc.nextLine();
-            if(text.equalsIgnoreCase("exit")) {
-            	break;
-            }
+            if(text.equalsIgnoreCase("exit") || (text.equals("")))
+            	break; //exit case
             //establish socket connection to server
-            socket = new Socket(host.getHostName(), 9876);
+            socket = new Socket(host.getHostName(), port);
             //write to socket using ObjectOutputStream
             oos = new ObjectOutputStream(socket.getOutputStream());
-            System.out.println("Sending request to Socket Server");
             oos.writeObject(text);
-            ois = new ObjectInputStream(socket.getInputStream());
-            String message = (String) ois.readObject();
-            System.out.println("Server: " + message);
-            ois.close();
-            oos.close();
-            socket.close();
-            Thread.sleep(100);
-            
-            System.out.println("Enter another test? y/n: ");
-            String more = sc.nextLine();
-            if ((more.contentEquals("n")) || (more.contentEquals("N")) || 
-            	(more.contentEquals("no")) || (more.contentEquals("No")) ||
-            	more.equalsIgnoreCase("exit"))
+            if(text.equalsIgnoreCase("terminate")) {
+            	System.out.println("\nTerminating Server!");
+            	oos.close();
+            	Thread.sleep(100);
             	break;
-        }
-        sc.close();
-        System.out.println("Exiting socket server.");
-        //establish socket connection to server
-        socket = new Socket(host.getHostName(), 9876);
-        oos = new ObjectOutputStream(socket.getOutputStream());
-        oos.writeObject("exit");
-        ois.close();
-        oos.close();
-        socket.close();
-        Thread.sleep(100);
-        System.out.println("Done");
+            } //server terminate request
+            else {
+            	System.out.println("Sending request to Socket Server");
+            	ois = new ObjectInputStream(socket.getInputStream());
+            	String message = (String) ois.readObject();
+            	System.out.println("Server: " + message + "\n");
+            	ois.close();
+            	oos.close();
+            	socket.close();
+            	Thread.sleep(100);
+            } //normal message output
+            
+        } //loop for client send requests
         
-    }
-}
+        System.out.println("Terminating Client!");
+        sc.close();
+        if(socket != null) 
+        	socket.close();
+        
+    } //end main
+   
+} //end Class
--- src/Server.java
+++ src/Server.java
@@ -2,37 +2,54 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.lang.ClassNotFoundException;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 /**
  * This class implements java Socket server
+ * Checks a message passed from a client for palindrome property (
+ * 	- ignores non alphanumeric chars
+ * @author David Kalytyuk
  * @author pankaj
- *
+ * original code from:
+ * https://www.journaldev.com/741/java-socket-programming-server-client
+ * modified for purposes of assignment
  */
 public class Server {
     
     //static ServerSocket variable
     private static ServerSocket server;
     //socket server port on which it will listen
-    private static int port = 9876;
+    private static int port = 1200; //default port number
     
     public static void main(String args[]) throws IOException, ClassNotFoundException{
+        if(args.length != 0) {
+	    	port = Integer.parseInt(args[0].substring(args[0].length() - 4));
+	    	if(port < 1024) {
+	    		System.out.println("\nCannot use ports below 1024, try again.");
+	    		System.exit(1);
+	    	} //specified port must be greater than 1024
+        } //specified port condition
+        System.out.println("\nPort Number: " + port + "\n");
         //create the socket server object
         server = new ServerSocket(port);
         //keep listens indefinitely until receives 'exit' call or program terminates
         while(true){
-            System.out.println("Waiting for the client request");
+            System.out.println("Waiting for the client request...");
             //creating socket and waiting for client connection
             Socket socket = server.accept();
             //read from socket to ObjectInputStream object
             ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
             //convert ObjectInputStream object to String
             String message = (String) ois.readObject();
-            System.out.println("Message Received: " + message);
-            //terminate the server if client sends exit request
-            if(message.equalsIgnoreCase("exit")) 
+            System.out.println("Message Received: " + message + "\n");
+            //terminate the server if client sends terminate request
+            if(message.equalsIgnoreCase("terminate")) { 
+            	ois.close();
+            	socket.close();
             	break;
+            } //server terminate condition
             //create ObjectOutputStream object
             ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
             //write object to Socket
@@ -44,11 +61,10 @@ public class Server {
             ois.close();
             oos.close();
             socket.close();
-        }
-        System.out.println("Shutting down Socket server!!");
-        //close the ServerSocket object
+        } //listening for input
+        System.out.println("Shutting down Socket server!");
         server.close();
-    }
+    } //end main
     
     private static boolean isPalindrome(String text) {
     	int start = 0;
@@ -69,4 +85,4 @@ public class Server {
     	return palindrome;
     } //end isPalindrome
     
-}
+} //end Class
